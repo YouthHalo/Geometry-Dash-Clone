6cd4538ea4f6a6d449acf6b14a608bb2944d09fd
YouthHalo <69648246+YouthHalo@users.noreply.github.com>
2024-08-12 14:17:44 -0400

    explosion effect, floor angle snapping

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
 
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -350.0
 
+@onready var speed = $"..".PLAYERSPEED
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var speed = $"..".PLAYERSPEED
-
+var falling = true
+func _ready():
+	velocity.x = speed
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		$Sprite2D.modulate = Color(0,1,0)
 		velocity.y += gravity * delta
+	if falling:
 		rotate(deg_to_rad(4))
 	if is_on_floor():
+		falling = false
 		$Sprite2D.modulate = Color(1,1,1)
-		#rotation = deg_to_rad(0)
+		rotation_degrees = round(rotation_degrees / 90) *90
 
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	velocity.x = speed
+		falling = true
+
 	if is_on_wall():
 		queue_free()
 	move_and_slide()
